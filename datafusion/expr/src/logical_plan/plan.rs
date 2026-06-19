155977c018a90ae599e4cbc736493294aaa3839d
Louis Vialar <louis.vialar@gmail.com>
2026-06-19 12:58:29 +0200

    fix: use full plan for build_join_schema
    this fixes an issue where mark joins "lose" the qualifier after optimize_projections in EXISTS subqueries. Indeed, in EXISTS subqueries, we don't need any projected field, so optimize_projections remove them all, which makes it impossible for build_join_schema to know what qualifier to use for the mark field

--- datafusion/expr/src/logical_plan/plan.rs
+++ datafusion/expr/src/logical_plan/plan.rs
@@ -666,8 +666,7 @@ impl LogicalPlan {
                 null_equality,
                 null_aware,
             }) => {
-                let schema =
-                    build_join_schema(left.schema(), right.schema(), &join_type)?;
+                let schema = build_join_schema(&left, &right, &join_type)?;
 
                 let new_on: Vec<_> = on
                     .into_iter()
@@ -910,7 +909,7 @@ impl LogicalPlan {
                 ..
             }) => {
                 let (left, right) = self.only_two_inputs(inputs)?;
-                let schema = build_join_schema(left.schema(), right.schema(), join_type)?;
+                let schema = build_join_schema(&left, &right, join_type)?;
 
                 let equi_expr_count = on.len() * 2;
                 assert!(expr.len() >= equi_expr_count);
@@ -3964,7 +3963,7 @@ impl Join {
         null_equality: NullEquality,
         null_aware: bool,
     ) -> Result<Self> {
-        let join_schema = build_join_schema(left.schema(), right.schema(), &join_type)?;
+        let join_schema = build_join_schema(&left, &right, &join_type)?;
 
         Ok(Join {
             left,
@@ -4016,8 +4015,8 @@ impl Join {
             .collect();
 
         let join_schema = build_join_schema(
-            left_sch.schema(),
-            right_sch.schema(),
+            &left_sch.build()?,
+            &right_sch.build()?,
             &original_join.join_type,
         )?;
 
